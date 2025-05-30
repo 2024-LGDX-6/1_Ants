97079142fe7f7de3164db779d2a490b51df0603e
김현도 <mrzhdrk7931@naver.com>
2025-05-30 17:14:43 +0900

    커스텀 레시피 목록, 정보 로직 생성
    custom_recipe_controller.dart
    custom_recipe_data.dart

--- taste_q/lib/controllers/custom_recipe_controller.dart
+++ taste_q/lib/controllers/custom_recipe_controller.dart
@@ -1,18 +1,86 @@
-class CustomRecipeDataDTO {
-  final List<int> customRecipeId;
-  final List<String> customReceipeName;
-  final List<String> customRecipeImageUrls;
-  final List<String> customIngredient;
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:taste_q/controllers/dto/custom_recipe_data_dto.dart';
+import 'package:taste_q/models/image_mapping.dart';
+import 'package:taste_q/providers/recipe_provider.dart';
 
-  CustomRecipeDataDTO({
-    required this.customRecipeId,
-    required this.customReceipeName,
-    required this.customRecipeImageUrls,
-    required this.customIngredient,
-  });
-
-}
+import '../models/recipe_mode.dart';
 
 class CustomRecipeController {
+
+  static const String baseUrl = "http://192.168.219.130:8000";
+
+  // 전체 개인 레시피 목록 불러오기
+  Future<CustomRecipeDataDTO> getAllCustomRecipes() async {
+    final response = await http.get(Uri.parse('$baseUrl/recipes')); // FastAPI 엔드포인트
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+
+      return CustomRecipeDataDTO.fromJson(jsonData, customRecipeImageMapping);
+    } else {
+      throw Exception('서버 오류: ${response.statusCode}');
+    }
+  }
+
+  // 개인 레시피 데이터 불러오기
+  Future<CustomRecipeDataDetailDto> getCustomRecipeData(
+      int recipeId, BuildContext context) async {
+    // 1. 개인 레시피 기본 정보 요청
+    final recipeResponse = await http.get(Uri.parse(
+        '$baseUrl/recipes/$recipeId'));
+    if (recipeResponse.statusCode != 200) {
+      throw Exception('레시피 정보를 불러올 수 없습니다.');
+    }
+    final recipeJson = json.decode(recipeResponse.body);
+
+    // 2. 개인 레시피 시즈닝 상세정보 요청
+    final detailResponse = await http.get(Uri.parse(
+        '$baseUrl/recipes/$recipeId/seasoning-details'));
+    if (detailResponse.statusCode != 200) {
+      throw Exception('레시피 조미료 정보를 불러올 수 없습니다.');
+    }
+    final List<dynamic> detailJson = json.decode(detailResponse.body);
+
+    // 3. 개인 레시피 이미지 경로 매핑
+    final imagePath = customRecipeImageMapping[recipeId] ?? 'default.jpg';
+
+    // 4. 현재 모드 가져오기 (Provider)
+    final mode = Provider.of<RecipeProvider>(context, listen: false).mode;
+
+    // 5. 모드에 따른 amounts 연산 후 변환
+    List<double> modifiedAmounts = detailJson.map((e) {
+      double originalAmount = (e['amount'] as num).toDouble();
+      switch (mode) {
+        case RecipeMode.wellness:
+          return originalAmount - (originalAmount * 0.1); // 웰빙모드: 1/10 빼기
+        case RecipeMode.gourmet:
+          return originalAmount + (originalAmount * 0.1); // 미식모드: 1/10 추가
+        case RecipeMode.standard:
+          return originalAmount; // 표준모드: 그대로
+      }
+    }).toList();
+
+    // 6. RecipeDataDTO 반환 (amounts를 변환값으로 교체)
+    return CustomRecipeDataDetailDto(
+      customRecipeId: recipeJson['custom_recipe_id'],
+      customRecipeName: recipeJson['custom_recipe_name'],
+      customRecipeImageUrl: imagePath,
+      seasoningNames: detailJson.map((e) => e['seasoning_name'] as String).toList(),
+      amounts: modifiedAmounts,
+    );
+  }
+
+  // RecipeModeSelector, SettingView에서 사용될 provider 메소드
+  void updateMode(BuildContext context, RecipeMode newMode) {
+    // Provider에서 모드 설정
+    Provider.of<RecipeProvider>(context, listen: false).setMode(newMode);
+  }
+
+  RecipeMode getCurrentMode(BuildContext context) {
+    return Provider.of<RecipeProvider>(context, listen: false).mode;
+  }
 
 }
