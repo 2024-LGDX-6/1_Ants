b91abb2749b8faf14ca15cfbf21c4bd990eff5f8
BSH <tmdtkr@naver.com>
2025-06-09 15:09:59 +0900

    제발 이제는 되겠지?

--- TasteQ_backend/src/service/nlp_service.py
+++ TasteQ_backend/src/service/nlp_service.py
@@ -1,7 +1,7 @@
 from eunjeon import Mecab
 
 # ✅ mecabrc 파일이 들어 있는 디렉토리만 경로로 사용
-mecab = Mecab("/usr/local/lib/mecab/mecab-ko-dic")
+mecab = Mecab()
 
 def extract_nouns(text: str) -> list[str]:
     return mecab.nouns(text)
