91ad6a87f5ec68dafd97e7469e7a9136678c7657
BSH <tmdtkr@naver.com>
2025-06-09 14:05:41 +0900

    메캅 경로 수정 2

--- TasteQ_backend/src/service/nlp_service.py
+++ TasteQ_backend/src/service/nlp_service.py
@@ -1,6 +1,6 @@
 from eunjeon import Mecab
 
-mecab = Mecab("/usr/local/lib/mecab/dic/mecab-ko-dic")  # ✅ 경로 명시
+mecab = Mecab("/usr/local/lib/mecab/mecab-ko-dic")  # ✅ 경로 명시
 
 def extract_nouns(text: str) -> list[str]:
     return mecab.nouns(text)
